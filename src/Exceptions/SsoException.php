71b1d31a346b8d2432180ca5cd671223a9d6f21e
LuRy <lury@lury.cz>
2026-03-22 14:07:31 +0100

    fix: add PKCE, timing-safe state, nonce validation, scope separator

--- src/Exceptions/SsoException.php
+++ src/Exceptions/SsoException.php
@@ -7,6 +7,6 @@ namespace NksHub\NetteSso\Exceptions;
 /**
  * Base exception for SSO authentication errors
  */
-class SsoException extends \Exception
+class SsoException extends \RuntimeException
 {
 }
